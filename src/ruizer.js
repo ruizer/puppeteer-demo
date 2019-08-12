0b909c598073ef06eecc0c8ace0f882b1691a49a
liaorui <liaorui@vsoontech.com>
2019-08-12 17:24:06 +0800

    format

--- src/ruizer.js
+++ src/ruizer.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    page.setViewport({width: 1200, height: 600})
+    page.setViewport({ width: 1200, height: 600 })
     // 这边测试博客地址绑定的是ruizer.github.io，加载可能非常慢，所以timeout设为了0
     await page.goto('https://www.ruizer.cn/', { timeout: 0, waitUntil: 'networkidle0' });
 
--- src/sf.js
+++ src/sf.js
@@ -1,19 +1,16 @@
 const puppeteer = require('puppeteer')
 const fs = require('fs')
-// var { timeout } = require('../tools/tools.js');
 
-var delay = 1000
 puppeteer.launch({ headless: false }).then(async browser => {
-    var page = await browser.newPage()
+    const page = await browser.newPage()
     page.setViewport({ width: 1200, height: 600 })
 
     /** 1. 到sf获取最新的前端文章 **/
     try {
         await page.goto('https://segmentfault.com/channel/frontend')
-        // await timeout(delay)
 
-        var SfArticleList = await page.evaluate(() => {
-            var list = [...document.querySelectorAll('.news-list .news__item-info>a')]
+        const SfArticleList = await page.evaluate(() => {
+            const list = [...document.querySelectorAll('.news-list .news__item-info>a')]
 
             return list.map(el => {
                 if (!el.hasChildNodes()) {
@@ -26,7 +23,7 @@ puppeteer.launch({ headless: false }).then(async browser => {
         fs.writeFile('./data/sf/sf.json', JSON.stringify(SfArticleList), (err) => {
             if (err) throw err;
             console.log('文件已被保存');
-          });
+        });
         console.log('SfArticleList:', SfArticleList);
 
         await page.screenshot({ path: './data/sf/sf.png', type: 'png' });
